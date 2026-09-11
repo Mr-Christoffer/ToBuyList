488867a2480a4ba7c8796f729d443e4f8dd7201b
Mr-Christoffer <ctsa.nilsson@gmail.com>
2026-09-11 12:06:16 +0200

    Improvements in listview

--- tobuylist.cs
+++ tobuylist.cs
@@ -1,13 +1,19 @@
 List<string> articles =new List<string>();
 List<int> prices = new List<int>();
-articles.Add("Hefj");
-articles.Add("He65j");
-articles.Add("He234j");
-prices.Add(25);
-prices.Add(54);
-prices.Add(65);
+articles.Add("Mjölk");
+articles.Add("Bröd");
+articles.Add("Ost");
+prices.Add(15);
+prices.Add(32);
+prices.Add(89);
+int totalCost = 0;
 
 for(int i = 0 ; i < prices.Count; i++)
 {
-    System.Console.WriteLine($"{articles[i]} \t {prices[i]}");
+    System.Console.WriteLine($"{i+1}. {articles[i]} \t\t {prices[i]} kr");
 }
+foreach (int p in prices)
+{
+    totalCost += p;
+}
+System.Console.WriteLine($"Totalt:\t\t{totalCost} kr");
