153cb9a548a5e0a927d61979d5aa9fab2fbcd1a0
Mr-Christoffer <ctsa.nilsson@gmail.com>
2026-09-11 12:25:56 +0200

    Added functionality for removing articles from list

--- tobuylist.cs
+++ tobuylist.cs
@@ -6,14 +6,37 @@ articles.Add("Ost");
 prices.Add(15);
 prices.Add(32);
 prices.Add(89);
-int totalCost = 0;
+bool programRun = true;
 
-for(int i = 0 ; i < prices.Count; i++)
+while (programRun)
 {
-    System.Console.WriteLine($"{i+1}. {articles[i], -15} {prices[i],5} kr"); //Added width paragraphs. -15 means take 15 slots
+    for(int i = 0 ; i < prices.Count; i++)
+{
+    System.Console.WriteLine($"{i+1}. {articles[i], -15} {prices[i],5} kr"); //Added width paragraphs. -15 means take 15 slots and adjust to left.
 }
+int totalCost = 0;
 foreach (int p in prices)
 {
     totalCost += p;
 }
 System.Console.WriteLine($"{"Totalt:",-12}{totalCost, 12} kr");
+
+System.Console.WriteLine("Skriv artikelnamn för att lägga till vara. Skriv ett artikelnummer för att ta bort en vara:");
+string? input = Console.ReadLine();
+//Determines if user types a word or number
+
+if (int.TryParse(input, out int number)) //If its a int-number
+    {
+        if(number <= articles.Count && number > 0) // If typed number exists as an articlenumber
+        {
+            articles.RemoveAt(number-1);
+            prices.RemoveAt(number-1);
+        }
+        else
+        {
+            System.Console.WriteLine("Det artikelnumret finns inte!");
+        }
+    }
+
+}
+
