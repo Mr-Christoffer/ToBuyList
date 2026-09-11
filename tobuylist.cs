dce729397597ccf5a291bd3862268b668a32ebb4
Mr-Christoffer <ctsa.nilsson@gmail.com>
2026-09-11 11:53:12 +0200

    Added a for-loop for showing the list

--- tobuylist.cs
+++ tobuylist.cs
@@ -1,2 +1,13 @@
-List<string> articles;
-List<int> prices;
+List<string> articles =new List<string>();
+List<int> prices = new List<int>();
+articles.Add("Hefj");
+articles.Add("He65j");
+articles.Add("He234j");
+prices.Add(25);
+prices.Add(54);
+prices.Add(65);
+
+for(int i = 0 ; i < prices.Count; i++)
+{
+    System.Console.WriteLine($"{articles[i]} \t {prices[i]}");
+}
